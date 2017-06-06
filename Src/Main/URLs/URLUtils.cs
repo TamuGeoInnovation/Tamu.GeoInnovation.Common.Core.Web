a521ca4204c5f869dcb22e2650e92644a65b2b6f
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2017-06-05 19:22:52 -0500

    updating getcontent

--- Src/Main/URLs/URLUtils.cs
+++ Src/Main/URLs/URLUtils.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Net;
 using System.Web;
-using System.IO;
+using System.Collections.Generic;
 
 namespace USC.GISResearchLab.Common.Utils.Web.URLs
 {
@@ -50,9 +50,166 @@ namespace USC.GISResearchLab.Common.Utils.Web.URLs
             return GetUrlContent(url, userName, password, cookies, userAgent, referrer, false);
         }
 
+
         public static string GetUrlContent(string url, string userName, string password, string cookies, string userAgent, string referrer, bool shouldGuessEncoding)
         {
+            return GetUrlContent(url, userName, password, cookies, null, userAgent, referrer, shouldGuessEncoding);
+        }
+
+        public static string GetUrlContentFromHttpWebResponse(HttpWebResponse response)
+        {
             string ret = null;
+            System.IO.Stream st = null;
+            System.IO.StreamReader sr = null;
+            try
+            {
+                st = response.GetResponseStream();
+
+                string charset = response.CharacterSet;
+
+                sr = new System.IO.StreamReader(st, System.Text.Encoding.GetEncoding(charset));
+
+
+                if (sr != null)
+                {
+                    ret = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error in GetUrlContent: " + e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+
+                if (st != null)
+                {
+                    st.Close();
+                }
+            }
+
+            return ret;
+        }
+
+        public static string GetUrlContent(string url, string userName, string password, string cookies, Dictionary<string, string> headers, string userAgent, string referrer, bool shouldGuessEncoding)
+        {
+            string ret = null;
+
+            try
+            {
+
+                HttpWebResponse response = GetUrlResponse(url, userName, password, cookies, headers, userAgent, referrer, shouldGuessEncoding);
+                ret = GetUrlContentFromHttpWebResponse(response);
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error in GetUrlContent: " + e.Message);
+            }
+
+            return ret;
+        }
+
+
+        public static HttpWebResponse GetUrlResponse(string url, string userName, string password, string cookies, Dictionary<string, string> headers, string userAgent, string referrer, bool shouldGuessEncoding)
+        {
+            HttpWebResponse ret = null;
+
+            if (shouldGuessEncoding)
+            {
+                return GetUrlResponseWithCharsetGuessing(url, userName, password, cookies, headers, userAgent, referrer, shouldGuessEncoding);
+            }
+            else
+            {
+
+                try
+                {
+                    System.Net.HttpWebRequest req = (HttpWebRequest)System.Net.HttpWebRequest.Create(url);
+
+                    if (userName != null && password != null)
+                    {
+                        req.Credentials = new System.Net.NetworkCredential(userName, password);
+                    }
+
+                    if (!String.IsNullOrEmpty(cookies))
+                    {
+                        if (req.CookieContainer == null)
+                        {
+                            req.CookieContainer = new CookieContainer();
+                            //((HttpWebRequest)req).CookieContainer.Add(new Cookie("domain", req.Address.DnsSafeHost));
+                        }
+
+                        string[] cookieList = cookies.Split(';');
+                        if (cookieList != null)
+                        {
+                            foreach (string cookiePair in cookieList)
+                            {
+                                string[] cookieParts = cookiePair.Split(new char[] { '=' }, 2);
+                                if (cookieParts != null)
+                                {
+                                    string name = cookieParts[0];
+                                    if (!String.IsNullOrEmpty(name))
+                                    {
+                                        name = name.Trim();
+                                    }
+
+                                    string val = cookieParts[1];
+                                    if (!String.IsNullOrEmpty(val))
+                                    {
+                                        val = val.Trim();
+                                    }
+
+                                    val = HttpUtility.UrlEncode(val);
+                                    ((HttpWebRequest)req).CookieContainer.Add(new Cookie(name, val) { Domain = req.Address.DnsSafeHost });
+                                }
+                            }
+                        }
+                    }
+
+                    if (!String.IsNullOrEmpty(userAgent))
+                    {
+                        ((HttpWebRequest)req).UserAgent = userAgent;
+                    }
+
+                    if (!String.IsNullOrEmpty(referrer))
+                    {
+                        ((HttpWebRequest)req).Referer = referrer;
+                    }
+
+                    ((HttpWebRequest)req).Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+                    ((HttpWebRequest)req).Headers.Add(HttpRequestHeader.AcceptCharset, "ISO-8859-1");
+
+                    if (headers != null)
+                    {
+                        foreach (KeyValuePair<string, string> entry in headers)
+                        {
+                            ((HttpWebRequest)req).Headers.Add(entry.Key, entry.Value);
+                        }
+
+                    }
+
+
+                    ret = (HttpWebResponse)req.GetResponse();
+
+
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Error in GetUrlResponse: " + e.Message);
+                    //return string.Empty;
+                }
+            }
+            return ret;
+        }
+
+
+        public static HttpWebResponse GetUrlResponseWithCharsetGuessing(string url, string userName, string password, string cookies, Dictionary<string, string> headers, string userAgent, string referrer, bool shouldGuessEncoding)
+        {
+            HttpWebResponse ret = null;
             System.IO.Stream st = null;
             System.IO.StreamReader sr = null;
             try
@@ -112,31 +269,19 @@ namespace USC.GISResearchLab.Common.Utils.Web.URLs
                 ((HttpWebRequest)req).Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
                 ((HttpWebRequest)req).Headers.Add(HttpRequestHeader.AcceptCharset, "ISO-8859-1");
 
+                if (headers != null)
+                {
+                    foreach (KeyValuePair<string, string> entry in headers)
+                    {
+                        ((HttpWebRequest)req).Headers.Add(entry.Key, entry.Value);
+                    }
 
-                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
-                //System.Net.WebResponse resp = req.GetResponse();
+                }
 
-               
 
-                st = response.GetResponseStream();
+                ret = (HttpWebResponse)req.GetResponse();
 
-                //MemoryStream memStream;
-                //using (Stream stream = response.GetResponseStream())
-                //{
-                //    memStream = new MemoryStream();
-
-                //    byte[] buffer = new byte[1024];
-                //    int byteCount;
-                //    do
-                //    {
-                //        byteCount = stream.Read(buffer, 0, buffer.Length);
-                //        memStream.Write(buffer, 0, byteCount);
-                //    } while (byteCount > 0);
-                //}
-
-                ////st.CopyTo(ms);
-
-                string charset = response.CharacterSet;
+                string charset = ret.CharacterSet;
 
                 if (shouldGuessEncoding)
                 {
@@ -212,27 +357,12 @@ namespace USC.GISResearchLab.Common.Utils.Web.URLs
                     ((HttpWebRequest)req2).Headers.Add(HttpRequestHeader.AcceptCharset, "ISO-8859-1");
 
 
-                    HttpWebResponse response2 = (HttpWebResponse)req2.GetResponse();
-
-                    if (st != null)
-                    {
-                        st.Close();
-                    }
-
-                    st = response2.GetResponseStream();
-                }
-
-                sr = new System.IO.StreamReader(st, System.Text.Encoding.GetEncoding(charset));
-                
-
-                if (sr != null)
-                {
-                    ret = sr.ReadToEnd();
+                    ret = (HttpWebResponse)req2.GetResponse();
                 }
             }
             catch (Exception e)
             {
-                throw new Exception("Error in GetUrlContent: " + e.Message);
+                throw new Exception("Error in GetUrlResponseWithCharsetGuessing: " + e.Message);
                 //return string.Empty;
             }
             finally
@@ -250,8 +380,6 @@ namespace USC.GISResearchLab.Common.Utils.Web.URLs
 
             return ret;
         }
-
-
 
         public static string getHostName()
         {

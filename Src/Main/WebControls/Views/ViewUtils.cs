3a0682fe2900769dc44ffc7d677ac58adaa9f0e9
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2020-02-27 16:51:53 -0600

    Remove Unused References

---
+++ Src/Main/WebControls/Views/ViewUtils.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Data;
+using System.Web.UI.WebControls;
+
+namespace Tamu.GeoInnovation.Common.Core.Web.WebControls.Views
+{
+    public class ViewUtils
+    {
+        public static void FillDropDownList(DropDownList d, DataTable dt, string textField, string valueField)
+        {
+            try
+            {
+                if (d != null)
+                {
+                    if (dt != null)
+                    {
+                        d.DataSource = dt;
+
+                        DataColumn dataColumnTextField = dt.Columns[textField];
+
+                        if (dataColumnTextField != null)
+                        {
+                            d.DataTextField = dataColumnTextField.ColumnName;
+
+                            DataColumn dataColumnValueField = dt.Columns[valueField];
+                            if (dataColumnValueField != null)
+                            {
+                                d.DataValueField = dataColumnValueField.ColumnName;
+                                d.DataBind();
+                                if (d.Items.Count > 0)
+                                {
+                                    d.SelectedIndex = 0;
+                                }
+                            }
+                            else
+                            {
+                                throw new Exception("FillDropDownList Error - Value field missing: " + valueField);
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("FillDropDownList Error - Text field missing: " + textField);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("FillDropDownList Error: " + e.Message, e);
+            }
+        }
+    }
+
+}
